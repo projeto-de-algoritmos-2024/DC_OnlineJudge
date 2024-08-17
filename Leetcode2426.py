3f4488a1dbc23229b20d1f95d57701665dfc6f1e
JoaoBarreto03 <joao.barreto@pagglo.com>
2024-08-17 19:13:51 -0300

    Codigo leetcode 2426 final

--- Leetcode2426.py
+++ Leetcode2426.py
@@ -3,8 +3,8 @@ from sortedcontainers import SortedList
 class Solution(object):
     def numberOfPairs(self, nums1, nums2, diff):
         n = len(nums1)
-        numsDiff = [nums1[i] - nums2[i] for i in range(n)]
-        return self.CountPairs(numsDiff, 0, n - 1, diff)
+        nums_diff = [nums1[i] - nums2[i] for i in range(n)]
+        return self.CountPairs(nums_diff, 0, n - 1, diff)
     
     def CountPairs(self, nums, left, right, diff):
         if left >= right:
@@ -14,13 +14,15 @@ class Solution(object):
         count = self.CountPairs(nums, left, mid, diff) + self.CountPairs(nums, mid + 1, right, diff)
         
         j = mid + 1
-        l = SortedList()
+        sorted_list = SortedList()
         
+        # Count pairs using the SortedList
         for i in range(mid, left - 1, -1):
             while j <= right and nums[j] <= nums[i] + diff:
-                l.add(nums[j])
+                sorted_list.add(nums[j])
                 j += 1
-            count += len(l) - l.bisect_right(nums[i] + diff)
+            count += len(sorted_list) - sorted_list.bisect_right(nums[i] + diff)
         
+        # Merge the two halves
         nums[left:right + 1] = sorted(nums[left:right + 1])
         return count
